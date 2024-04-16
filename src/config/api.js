eda1cb5ad912d90f0a8a51ddb55d4475706082d9
santanu1998 <santanu.singha021@gmail.com>
2024-04-16 13:38:40 +0530

    final commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // export const API_URL = "http://localhost:8080";
-export const API_URL = "https://51.21.41.40:8080";
+export const API_URL = "http://51.21.41.40:8081";
 
 
 export const api = axios.create({
